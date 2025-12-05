143bb959e4aa69d998cf2691e41b878dab553bae
Luis <9101860@alumnos.ufv.es>
2025-12-05 21:18:36 +0100

    probando pipeline

--- relecloud/views.py
+++ relecloud/views.py
@@ -18,7 +18,7 @@ def index(request):
     return render(request, 'index.html')
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about.html') 
 
 def destinations(request):
     all_destinations = Destination.objects.all()
